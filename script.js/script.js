d13e9921a9b7237fecb009122043238a9a5a4150
Sanskark28 <sanskarkhandelwal28032006@gmail.com>
2026-03-01 02:15:56 +0530

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,17 @@
+function joinNow(){
+alert("Welcome! Our team will contact you soon.");
+}
+
+function submitForm(e){
+e.preventDefault();
+alert("Thank you! We will contact you soon.");
+}
+
+function openWhatsApp(){
+
+window.open(
+"https://wa.me/919876543210",
+"_blank"
+);
+
+}
